02c9b77465fb457df88408fbaca716e4f2ecddc0
anarizollacaa@gmail.com <anarizollacaa@gmail.com>
2023-03-27 19:50:59 -0600

    Se terminó el ejercicio 2 y se avanzó 3/4 ejercicio 3

--- JS-04 - Arrays and Loops/JS/main.js
+++ JS-04 - Arrays and Loops/JS/main.js
@@ -1,3 +1,4 @@
+console.log("*************** EJERCICIO 1 *************");
 //Parte 1
 let nombres = ["Sofía","David","Juan"];
 
@@ -5,18 +6,54 @@ let divAlert = document.getElementById("divAlert");
 
 //parte 2
 nombres.push("Sara","Augustin");
-//nombres.shift();
+nombres.shift();
 
 //Parte 1
 divAlert.innerHTML += "<ol><li>" + nombres[0] + "</li>"
 +"<li>" + nombres[1] + "</li>"
 +"<li>" + nombres[2] + "</li>"
-+"<li>" + nombres[3] + "</li>"
-+"<li>" + nombres[4] + "</li>"
-+"<li>" + nombres[5] + "</li></ol>";
++"<li>" + nombres[3] + "</li></ol>";
 
 //Parte 3
-
-/*let nombres = ["Sofía", "David", "Juan"];
-nombres.splice(0,1,"Renata",;
+nombres.splice(1,0,"Renata");
+nombres.push("Elena");
 console.log(nombres);
+
+//Parte 2
+for (index = 0; index<nombres.length; index ++){
+    console.log(nombres[index]);
+}
+
+console.log("*************** EJERCICIO 2 *************");
+
+
+   let asteriscos = [];
+   for (let index = 0; index <= 5; index++) {
+        asteriscos.push("*");
+        console. log (asteriscos.join(" "));
+    }
+
+console.log("*************** EJERCICIO 3 *************");
+
+console.log("Parte 1");
+for (let xValue = 5; xValue > 0; xValue-=0.5) {
+    console.log(xValue);
+}
+
+console.log("Parte 2");
+for (let index = 1; index < 100; index+=2) {
+    console.log(index); 
+}
+
+console.log("Parte 3");
+
+let print = [];
+let valor= 1;
+let n = 10;
+
+while (n>=1){
+    print.push("["+valor+"]");
+    n--;
+    valor++;
+}
+console.log(print.join(" "));
